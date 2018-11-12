80e92af8c5523c9a78732f0bfff7ae2c135a81f5
br00klyn93 <bmclaury93@gmail.com>
2018-11-11 18:24:30 -0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,10 @@
 <?php
-$a = $_GET["link"];
-$homepage = file_get_contents($a);
-echo $homepage;
-
+echo "[
+    {
+        "userId": 1,
+        "id": 1,
+        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    }
+]";
+?>
